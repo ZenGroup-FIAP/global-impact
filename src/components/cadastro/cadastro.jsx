b517c7a83742ee8cf1826dd36828ead167e3be59
Matheus Cruz <mccruz2611@gmail.com>
2021-11-12 18:40:20 -0300

    criando o corpo do projeto

---
+++ src/components/cadastro/cadastro.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export default function cadastro() {
+    return (
+        <div>
+            <div>
+                <img src="null" alt="Logo do site"/>
+            </div>
+
+            <div>
+                <div>
+                    <p>usuario</p>
+                    <form action=""></form>
+                </div>
+
+                <div>
+                    <p>senha</p>
+                    <form action=""></form>
+                </div>
+
+                <div>
+                    <p>Supermecado ou unidade</p>
+                    <form action=""></form>
+                </div>
+
+                <a href="null">CONFIRMAR</a>
+                
+            </div>
+        </div>
+    )
+}
