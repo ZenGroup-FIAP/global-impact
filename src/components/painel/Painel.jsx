969513cadfa426a4577fd380d7bae4850afd8be0
MFTECH-code <51761169+MFTECH-code@users.noreply.github.com>
2021-11-18 10:42:00 -0300

    Consumer API delete supermercado

--- src/components/painel/Painel.jsx
+++ src/components/painel/Painel.jsx
@@ -19,6 +19,16 @@ export default function Painel() {
             console.log(erro)
         }),[])
 
+    const handleDelete = (id) => {
+        fetch("/rest/supermercado/"+id, {
+            method: "delete"
+        }).then(() => {
+            window.location = "/"
+        }).catch((erro) => {
+            console.log(erro)
+        })
+    }
+
 
     if (alimentos.length === 0) {
         selected = false
@@ -62,7 +72,7 @@ export default function Painel() {
                                 setAlimentos(mercado.alimentos)
                                 }}>Alimentos</button></FlexBoxListagemItem>
                             <FlexBoxListagemItem>
-                                <AcoesBtn className="AcoesBtn"><RiDeleteBin2Fill /></AcoesBtn>
+                                <Link className="AcoesBtn" to="/" onClick={() => handleDelete(mercado.id)}><RiDeleteBin2Fill /></Link>
                                 <Link className="AcoesBtn" to="/formsupermercados"><RiEdit2Fill /></Link>
                             </FlexBoxListagemItem>
                         </FlexBoxListagem>
